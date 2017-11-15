1a692a7bb45fc983452e8e5574e0f78db22b0a83
Ed <edg@privacyinternational.org>
2017-11-15 13:33:52 +0000

    Remove unused bridge_port from ifaces

--- Router.java
+++ Router.java
@@ -219,7 +219,6 @@ public class Router extends AStructuredProfile {
 				units.addElement(model.getServerModel(server).getInterfaceModel().addIface(srv.replaceAll("-", "_") + "_router_iface",
 																							"static",
 																							model.getData().getIface(server) + ((!srv.equals(server)) ? ":" + model.getData().getSubnet(srv) : ""),
-																							null,
 																							gateway,
 																							model.getData().getNetmask(),
 																							null,
@@ -240,7 +239,6 @@ public class Router extends AStructuredProfile {
 					units.addElement(model.getServerModel(server).getInterfaceModel().addIface(device.replaceAll("-", "_") + "_router_iface_" + i,
 																								"static",
 																								model.getData().getIface(server) + ":" + subnet + i,
-																								null,
 																								gateways[i],
 																								model.getData().getNetmask(),
 																								null,
@@ -389,6 +387,11 @@ public class Router extends AStructuredProfile {
 			//Log anything hopping to our egress chain
 			fm.addFilter(cleanServerName + "_log_egress_traffic", egressChain,
 					"-j LOG --log-prefix \\\"ipt-" + cleanServerName + ": \\\"");
+			//Allow related ingress traffic
+			fm.addFilter(cleanServerName + "_allow_related_ingress_traffic", ingressChain,
+					"-p tcp"
+					+ " -m state --state ESTABLISHED,RELATED"
+					+ " -j ACCEPT");
 
 		}
 	
@@ -655,7 +658,6 @@ public class Router extends AStructuredProfile {
 																						null,
 																						null,
 																						null,
-																						null,
 																						null));
 
 			String dhclient = "option rfc3442-classless-static-routes code 121 = array of unsigned integer 8;\n";
@@ -688,7 +690,6 @@ public class Router extends AStructuredProfile {
 				units.addElement(model.getServerModel(server).getInterfaceModel().addIface("router_ext_static_iface_" + i,
 																							"static",
 																							model.getData().getExtIface(server),
-																							null,
 																							address,
 																							netmask,
 																							broadcast,
--- Service.java
+++ Service.java
@@ -91,7 +91,6 @@ public class Service extends AStructuredProfile {
 		units.addElement(model.getServerModel(server).getInterfaceModel().addIface(server.replace("-", "_") + "_primary_iface",
 																				   "static",
 																				   model.getData().getIface(server),
-																				   null,
 																				   model.getServerModel(server).getIP(),
 																				   model.getData().getNetmask(),
 																				   null,
