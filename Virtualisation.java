6d814f8ffb0aa76fb1bb8c955c78bb22515f0286
Ed Geraghty <edg@privacyinternational.org>
2018-01-09 12:06:25 +0000

    Make sure prerequisites for VBox are installed on metal

--- Virtualisation.java
+++ Virtualisation.java
@@ -27,6 +27,9 @@ public class Virtualisation extends AStructuredProfile {
 		model.getServerModel(server).getAptSourcesModel().addAptSource(server, model, "virtualbox", "proceed", "deb http://download.virtualbox.org/virtualbox/debian stretch contrib", "keyserver.ubuntu.com", "0xa2f683c52980aecf");
 		model.getServerModel(server).addRouterFirewallRule(server, model, "allow_virtualbox", "download.virtualbox.org", new String[]{"80"});
 
+		units.addElement(new InstalledUnit("build_essential", "build-essential"));
+		units.addElement(new InstalledUnit("linux_headers", "build_essential_installed", "linux-headers-$(uname -r)"));
+		
 		units.addElement(new InstalledUnit("metal_virtualbox", "virtualbox_gpg", "virtualbox-5.2"));
 		units.addElement(new InstalledUnit("metal_genisoimage", "genisoimage"));
 		units.addElement(new InstalledUnit("metal_rsync", "rsync"));
