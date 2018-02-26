817968e465f8d62d2d34a32a386145557a888f51
Ed Geraghty <edg@privacyinternational.org>
2018-02-26 16:29:48 +0000

    Create msmtp profile

---
+++ Msmtp.java
@@ -0,0 +1,74 @@
+package profile;
+
+import java.util.Vector;
+
+import core.iface.IUnit;
+import core.model.NetworkModel;
+import core.profile.AStructuredProfile;
+import core.unit.SimpleUnit;
+import core.unit.pkg.InstalledUnit;
+
+public class Msmtp extends AStructuredProfile {
+	
+	public Msmtp() {
+		super("msmtp");
+	}
+
+	protected Vector<IUnit> getInstalled(String server, NetworkModel model) {
+		Vector<IUnit> units = new Vector<IUnit>();
+		
+		units.addAll(model.getServerModel(server).getBindFsModel().addLogBindPoint(server, model, "msmtp", "proceed", "nginx", "0755"));
+
+		units.addElement(new InstalledUnit("msmtp", "proceed", "msmtp"));
+		units.addElement(new InstalledUnit("ca_certificates", "proceed", "ca-certificates"));
+		
+		return units;
+	}
+	
+	protected Vector<IUnit> getPersistentConfig(String server, NetworkModel model) {
+		Vector<IUnit> units =  new Vector<IUnit>();
+		
+		
+		return units;
+	}
+
+	protected Vector<IUnit> getLiveConfig(String server, NetworkModel model) {
+		Vector<IUnit> units = new Vector<IUnit>();
+		
+		String msmtprc = "";
+		msmtprc += "account        default\n";
+		msmtprc += "host           <SMTP HOST>\n";
+		msmtprc += "port           25\n";
+		msmtprc += "from           <FROM EMAIL ADDRESS>\n";
+		msmtprc += "user           <SMTP USERNAME>\n";
+		msmtprc += "password       <SMTP PASSWORD>\n";
+		msmtprc += "auth           on\n";
+		msmtprc += "tls            on\n";
+		msmtprc += "tls_trust_file /etc/ssl/certs/ca-certificates.crt\n";
+		msmtprc += "logfile        /var/log/msmtp/msmtp.log";
+		
+		units.addElement(new SimpleUnit("msmtprc_exists", "msmtp_installed",
+				"",
+				"sudo [ -f \"/etc/msmtprc\" ] && echo pass || echo fail", "pass", "pass",
+				"You need to create the file /etc/msmtprc, using the following format:\n" + msmtprc));
+		
+		return units;
+	}
+
+	protected Vector<IUnit> getPersistentFirewall(String server, NetworkModel model) {
+		Vector<IUnit> units = new Vector<IUnit>();
+		
+		String cleanName   = server.replaceAll("-",  "_");
+		String egressChain = cleanName + "_egress";
+		
+		//Allow email capability
+		for (String router : model.getRouters()) {
+			model.getServerModel(router).getFirewallModel().addFilter(server + "_allow_email", egressChain,
+				"-p tcp"
+				+ " --dport 25"
+				+ " -j ACCEPT");
+		}
+		
+		return units;
+	}
+}
