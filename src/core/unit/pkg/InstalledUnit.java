194c2d460badb50f43edafb2479d1c683c8204c3
Ed Geraghty <edg@privacyinternational.org>
2018-08-21 12:41:53 +0100

    Reorder constructors so they can be overloaded properly

--- src/core/unit/pkg/InstalledUnit.java
+++ src/core/unit/pkg/InstalledUnit.java
@@ -4,30 +4,19 @@ import core.unit.SimpleUnit;
 
 public class InstalledUnit extends SimpleUnit {
 
-	public InstalledUnit(String name, String pkg) {
-		super(name + "_installed", "proceed",
-				"export DEBIAN_FRONTEND=noninteractive; "
-				+ "sudo apt-get update;"
-				+ "sudo -E apt-get install --assume-yes " + pkg + ";",
-				"dpkg-query --status " + pkg + " | grep \"Status:\";", "Status: install ok installed", "pass",
-				"Couldn't install " + pkg + ".  This is pretty serious.");
-	}
-
-	public InstalledUnit(String name, String precondition, String pkg) {
-		super(name + "_installed", precondition,
-				"export DEBIAN_FRONTEND=noninteractive; "
-				+ "sudo apt-get update;"
-				+ "sudo -E apt-get install --assume-yes " + pkg + ";",
-				"dpkg-query --status " + pkg + " | grep \"Status:\";", "Status: install ok installed", "pass",
-				"Couldn't install " + pkg + ".  This is pretty serious.");
-	}
-
 	public InstalledUnit(String name, String precondition, String pkg, String message) {
 		super(name + "_installed", precondition,
 				"export DEBIAN_FRONTEND=noninteractive; "
 				+ "sudo apt-get update;"
 				+ "sudo -E apt-get install --assume-yes " + pkg + ";",
-				"dpkg-query --status " + pkg + " | grep \"Status:\";", "Status: install ok installed", "pass",
-				message);
 	}
+
+	public InstalledUnit(String name, String pkg) {
+		this(name, "proceed", pkg, "Couldn't install " + pkg + ".  This is pretty serious.");
+	}
+
+	public InstalledUnit(String name, String precondition, String pkg) {
+		this(name, precondition, pkg, "Couldn't install " + pkg + ".  This is pretty serious.");
+	}
+
 }
