085e57f7617d688443d3d675cfcc31333bfb7b3b
Ed Geraghty <edg@privacyinternational.org>
2018-10-18 16:55:06 +0100

    Change ipFromClass scope from private to public
    This allows us to arbitrarily build VPN interfaces on a given router without needing a full user declaration

--- src/core/model/DeviceModel.java
+++ src/core/model/DeviceModel.java
@@ -35,7 +35,7 @@ public class DeviceModel extends AModel {
 		return units;
 	}
 
-	private String ipFromClass() {
+	public String ipFromClass() {
 		String subnet  = get3rdOctet();
 		String ipClass = networkData.getIPClass();
 		
