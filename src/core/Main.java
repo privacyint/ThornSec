ad167ca9126aa3ddd831ff7f57c0e0f7848bd404
Ed Geraghty <edg@privacyinternational.org>
2019-03-29 13:24:39 +0000

    Give our Main class a file chooser if we don't pass arg[0]

--- src/core/Main.java
+++ src/core/Main.java
@@ -4,13 +4,38 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import core.model.ThornsecModel;
 import core.view.FullFrame;
 
 public class Main {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args [0] - Path to our JSON
+	 * @throws Exception Cannot read the JSON file
+	 */
 	public static void main(String[] args) throws Exception {
-		String text = new String(Files.readAllBytes(Paths.get(args[0])), StandardCharsets.UTF_8)
+		String jsonPath = null;
+		
+		if (args.length == 0) { 
+		    JFileChooser chooser = new JFileChooser();
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON Config File", "json");
+		    chooser.setFileFilter(filter);
+		    int returnVal = chooser.showOpenDialog(null);
+		    
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		       jsonPath = chooser.getSelectedFile().getPath();
+		    }
+		}
+		else {
+			jsonPath = args[0];
+		}
+		
+		String text = new String(Files.readAllBytes(Paths.get(jsonPath)), StandardCharsets.UTF_8)
 				.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
 
 		ThornsecModel model = new ThornsecModel();
