331acc5dfb65a01b306c947d8279c4127401c9b1
Ed <ed@geraghty.london>
2019-06-21 18:33:52 +0100

    Add a few more exceptions...

---
+++ src/core/exception/data/InvalidDiskSizeException.java
@@ -0,0 +1,5 @@
+package core.exception.data;
+
+public class InvalidDiskSizeException extends ADataException {
+	private static final long serialVersionUID = -4978682429685931190L;
+}
---
+++ src/core/exception/runtime/InvalidNICException.java
@@ -0,0 +1,5 @@
+package core.exception.runtime;
+
+public class InvalidNICException extends ARuntimeException {
+	private static final long serialVersionUID = 2656177660603769643L;
+}
