fb8867d454d2067594b63c03a793341aece45299
Ed <ed@geraghty.london>
2019-07-15 13:36:03 +0100

    Full rewrite of DiskData

--- src/core/data/machine/configuration/DiskData.java
+++ src/core/data/machine/configuration/DiskData.java
@@ -1,8 +1,8 @@
 /*
  * This code is part of the ThornSec project.
- * 
- * To learn more, please head to its GitHub rep: @privacyint
- * 
+ *
+ * To learn more, please head to its GitHub repo: @privacyint
+ *
  * Pull requests encouraged.
  */
 package core.data.machine.configuration;
@@ -14,7 +14,6 @@ import javax.json.JsonObject;
 import javax.json.stream.JsonParsingException;
 
 import core.data.AData;
-
 import core.exception.data.ADataException;
 import core.exception.data.machine.InvalidDiskSizeException;
 
@@ -27,16 +26,15 @@ public class DiskData extends AData {
 	 * You're either a Disk or you're a DVD.
 	 */
 	public enum Medium {
-		
-		DISK("disk"),
-		DVD("dvd");
-		
+
+		DISK("disk"), DVD("dvd");
+
 		private String medium;
-		
+
 		Medium(String medium) {
 			this.medium = medium;
 		}
-		
+
 		public String getMedium() {
 			return this.medium;
 		}
@@ -46,80 +44,91 @@ public class DiskData extends AData {
 	 * As far as we're concerned, there are only three file formats.
 	 */
 	public enum Format {
-		VDI("vdi"),
-		VMDK("vmdk"),
-		VHD("vhd");
-		
+		VDI("vdi"), VMDK("vmdk"), VHD("vhd");
+
 		private String format;
-		
+
 		Format(String format) {
 			this.format = format;
 		}
-		
+
 		public String getFormat() {
 			return this.format;
 		}
 	}
-	
+
 	private Medium medium;
 	private Format format;
 	private File filename;
 	private Integer size;
-	private File diffparent;
+	private File diffParent;
 	private String comment;
-	
-	/**
-	 * Creates a new DiskData full of null values.
-	 * 
-	 * Please call read() if instantiating this way!
-	 */
-	public DiskData()
-	throws InvalidDiskSizeException {
-		this(null, null, null, null, null, null);
+
+	public DiskData(String label) throws InvalidDiskSizeException {
+		super(label);
+
+		this.medium = null;
+		this.format = null;
+		this.filename = null;
+		this.size = null;
+		this.diffParent = null;
+		this.comment = null;
 	}
-	
-	/**
-	 * @param medium disk|dvd
-	 * @param format vdi|vmdk|vhd
-	 * @param filename
-	 * @param size disk size in megabytes
-	 * @param diffparent
-	 * @param comment
-	 * @throws InvalidDiskSizeException
-	 */
-	public DiskData(Medium medium, Format format, File filename, Integer size, File diffparent, String comment)
-	throws InvalidDiskSizeException {
-		super("disk");
-		
-		this.medium     = medium;
-		this.format     = format;
-		this.filename   = filename;
-		
-		if (size <= 0) {
+
+	@Override
+	public void read(JsonObject data) throws ADataException, JsonParsingException, IOException {
+		if (data.containsKey("medium")) {
+			setMedium(Medium.valueOf(data.getString("medium")));
+		}
+		if (data.containsKey("format")) {
+			setFormat(Format.valueOf(data.getString("format")));
+		}
+		if (data.containsKey("filename")) {
+			setFilename(new File(data.getString("filename")));
+		}
+		if (data.containsKey("diffparent")) {
+			setdiffParent(new File(data.getString("diffparent")));
+		}
+		if (data.containsKey("comment")) {
+			setComment(data.getString("comment"));
+		}
+		if (data.containsKey("size")) {
+			setSize(data.getInt("size"));
+		}
+	}
+
+	private void setSize(int size) throws InvalidDiskSizeException {
+		if (size < 512) {
 			throw new InvalidDiskSizeException();
 		}
-		else {
-			this.size = size;
-		}
-		
-		this.diffparent = diffparent;
-		this.comment    = comment;
+
+		this.size = size;
 	}
-	
-	@Override
-	public void read(JsonObject data)
-	throws ADataException, JsonParsingException, IOException {
-		this.medium     = Medium.valueOf(data.getString("medium", null));
-		this.format     = Format.valueOf(data.getString("format", null));
-		this.filename   = new File(data.getString("filename", null));
-		this.diffparent = new File(data.getString("filename", null));
-		this.comment    = data.getString("comment", null);
+
+	private void setComment(String comment) {
+		this.comment = comment;
 	}
-	
+
+	private void setFilename(File filename) {
+		this.filename = filename;
+	}
+
+	private void setdiffParent(File diffParent) {
+		this.diffParent = diffParent;
+	}
+
+	private void setFormat(Format format) {
+		this.format = format;
+	}
+
+	private void setMedium(Medium medium) {
+		this.medium = medium;
+	}
+
 	public Medium getMedium() {
 		return this.medium;
 	}
-	
+
 	public Format getFormat() {
 		return this.format;
 	}
@@ -133,11 +142,11 @@ public class DiskData extends AData {
 	}
 
 	public File getDiffparent() {
-		return this.diffparent;
+		return this.diffParent;
 	}
 
 	public String getComment() {
 		return this.comment;
 	}
-	
+
 }
