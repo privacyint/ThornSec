e117901ba384686ea0240e68f289fa996a107667
Ed Geraghty <edg@privacyinternational.org>
2019-07-17 10:35:20 +0100

    getNetworking() always was a really ugly hack
    Bye!!

--- src/core/profile/AProfile.java
+++ src/core/profile/AProfile.java
@@ -1,17 +1,13 @@
 /*
  * This code is part of the ThornSec project.
- * 
+ *
  * To learn more, please head to its GitHub repo: @privacyint
- * 
+ *
  * Pull requests encouraged.
  */
 package core.profile;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import core.iface.IProfile;
-import core.iface.IUnit;
 import core.model.network.NetworkModel;
 
 public abstract class AProfile implements IProfile {
@@ -31,9 +27,5 @@ public abstract class AProfile implements IProfile {
 
 	public final NetworkModel getNetworkModel() {
 		return this.networkModel;
-	}
-
-	public Set<IUnit> getNetworking() {
-		return new HashSet<>();
 	}
 }
