8fa9ea75375f758ea511c22ba3761008f528f03c
Ed <ed@geraghty.london>
2019-07-18 14:24:13 +0100

    Start fleshing out Dedicated

--- src/profile/type/Dedicated.java
+++ src/profile/type/Dedicated.java
@@ -14,8 +14,11 @@ import core.exception.runtime.InvalidServerModelException;
 import core.iface.IUnit;
 import core.model.network.NetworkModel;
 import core.profile.AStructuredProfile;
-import core.unit.fs.DirUnit;
 
+/**
+ * This is a dedicated server on your network. This is something ThornSec needs
+ * to know about, but shouldn't attempt to configure
+ */
 public class Dedicated extends AStructuredProfile {
 
 	public Dedicated(String label, NetworkModel networkModel) throws InvalidServerModelException {
@@ -23,62 +26,19 @@ public class Dedicated extends AStructuredProfile {
 
 		networkModel.getServerModel(label).setFirstOctet(10);
 		networkModel.getServerModel(label).setSecondOctet(0);
-//TODO: fixme
-		// networkModel.getServerModel(label)
-		// .setThirdOctet(networkModel.getDediServers().indexOf(networkModel.getServerModel(label))
+		// TODO: fixme
+		// setThirdOctet(networkModel.getDediServers().indexOf(networkModel.getServerModel(label))
 		// + 1);
-	}
-
-	@Override
-	protected Set<IUnit> getPersistentConfig() {
-		final Set<IUnit> units = new HashSet<>();
-
-		// Create /media/metaldata & /media/data bindfs point
-		units.add(new DirUnit("metaldata_bindpoint", "proceed", "/media/metaldata"));
-		units.add(new DirUnit("data_dir_exists", "proceed", "/media/data/"));
-
-		return units;
 	}
 
 	@Override
 	public Set<IUnit> getPersistentFirewall() throws InvalidServerModelException {
 		final Set<IUnit> units = new HashSet<>();
 
-		this.networkModel.getServerModel(getLabel()).addEgress("cdn.debian.net");
-		this.networkModel.getServerModel(getLabel()).addEgress("security-cdn.debian.org");
-		this.networkModel.getServerModel(getLabel()).addEgress("prod.debian.map.fastly.net");
-		this.networkModel.getServerModel(getLabel()).addEgress("download.virtualbox.org");
-
-		/*
-		 * Hashtable<String, InterfaceData> lanIfaces =
-		 * networkModel.getData().getLanIfaces(getLabel()); if (lanIfaces.isEmpty()) {
-		 * JOptionPane.showMessageDialog(null,
-		 * "You must declare at least one lan interface for \"" + getLabel() +
-		 * ".\n\nFormat is:\n\"lan\":[{\"interfacename\":\"macaddress\"}]");
-		 * System.exit(1); } else {
-		 *
-		 *
-		 * int i = 0;
-		 *
-		 * for (Map.Entry<String, String> lanIface : lanIfaces.entrySet() ) {
-		 * InterfaceModel im = me.getLanInterfaces();
-		 *
-		 * InetAddress subnet = networkModel.stringToIP(me.getFirstOctet() + "." +
-		 * me.getSecondOctet() + "." + me.getThirdOctet() + "." + (i * 4)); InetAddress
-		 * router = networkModel.stringToIP(me.getFirstOctet() + "." +
-		 * me.getSecondOctet() + "." + me.getThirdOctet() + "." + ((i * 4) + 1));
-		 * InetAddress address = networkModel.stringToIP(me.getFirstOctet() + "." +
-		 * me.getSecondOctet() + "." + me.getThirdOctet() + "." + ((i * 4) + 2));
-		 * InetAddress broadcast = networkModel.stringToIP(me.getFirstOctet() + "." +
-		 * me.getSecondOctet() + "." + me.getThirdOctet() + "." + ((i * 4) + 3));
-		 * InetAddress netmask = networkModel.getData().getNetmask();
-		 *
-		 * im.addIface(new InterfaceData(getLabel(), lanIface.getKey(),
-		 * lanIface.getValue(), "static", null, subnet, address, netmask, broadcast,
-		 * router, "comment goes here") );
-		 *
-		 * ++i; } }
-		 */
+		this.networkModel.getServerModel(getLabel()).addEgress("cdn.debian.net:80,443");
+		this.networkModel.getServerModel(getLabel()).addEgress("security-cdn.debian.org:80,443");
+		this.networkModel.getServerModel(getLabel()).addEgress("prod.debian.map.fastly.net:80,443");
+		this.networkModel.getServerModel(getLabel()).addEgress("download.virtualbox.org:80,443");
 
 		return units;
 	}
