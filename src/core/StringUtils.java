a22b3844dce4264ff9be9a0134b52aac02e6a9b0
Ed <ed@geraghty.london>
2019-08-05 18:05:07 +0100

    Add a method to parse free-form sizes into an Integer of MB

--- src/core/StringUtils.java
+++ src/core/StringUtils.java
@@ -1,5 +1,10 @@
 package core;
 
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtils {
 	
 	/**
@@ -13,6 +18,38 @@ public class StringUtils {
 		String invalidChars = "[^a-zA-Z0-9]";
 		
 		return toClean.replaceAll(invalidChars, replacement);
-
 	}
+
+	/**
+	 * Loosely based on https://stackoverflow.com/a/12090634
+	 *
+	 * @param freeformValue String representation of the value
+	 * @return the value in megabytes, (rounded to the nearest whole number)
+	 */
+	public static Integer stringToMegaBytes(String freeformValue) {
+		Integer returnValue = null;
+
+		final Pattern patt = Pattern.compile("([\\d,]+)([GMK])", Pattern.CASE_INSENSITIVE);
+		final Matcher matcher = patt.matcher(freeformValue.trim());
+		final Map<String, Integer> powers = Map.of("G", 3, "M", 2, "K", 1);
+
+		if (matcher.find()) {
+			final String number = matcher.group(1);
+			final int power = powers.get(matcher.group(2).toUpperCase());
+			final BigInteger bytes = new BigInteger(number).multiply(BigInteger.valueOf(1024).pow(power));
+
+			returnValue = bytes.divide(BigInteger.valueOf(1024).pow(2)).intValueExact();
+		}
+
+		assert (stringToMegaBytes("1.2g") == 1229);
+		assert (stringToMegaBytes("1,4g") == 1434); // We handle that delimiter too
+		assert (stringToMegaBytes("0.1MB") == 0);
+		assert (stringToMegaBytes("756k") == 1);
+		assert (stringToMegaBytes("10g") == 10240);
+		assert (stringToMegaBytes("abc") == null);
+		assert (stringToMegaBytes("10G") == 10240);
+
+		return returnValue;
+	}
+
 }
