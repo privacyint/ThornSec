e5bdbdcd9f108b0055ea2e327d8c63afaf7ea2dd
Ed <ed@geraghty.london>
2019-08-05 12:37:39 +0100

    Add getNetworkModel() to AModel
    Also add some comments whilst I'm in there

--- src/core/model/AModel.java
+++ src/core/model/AModel.java
@@ -1,23 +1,38 @@
+/*
+ * This code is part of the ThornSec project.
+ *
+ * To learn more, please head to its GitHub repo: @privacyint
+ *
+ * Pull requests encouraged.
+ */
 package core.model;
 
+import core.exception.runtime.ARuntimeException;
 import core.model.network.NetworkModel;
 
-import core.exception.runtime.ARuntimeException;
-
+/**
+ * This class represents a Model of some type.
+ *
+ * A Model is what we build from Data objects.
+ */
 public abstract class AModel {
 
 	protected String label;
 	protected NetworkModel networkModel;
 
 	protected AModel(String label, NetworkModel networkModel) {
-		this.label        = label;
+		this.label = label;
 		this.networkModel = networkModel;
 	}
-	
+
 	public final String getLabel() {
 		return this.label;
 	}
 
+	public final NetworkModel getNetworkModel() {
+		return this.networkModel;
+	}
+
 	public void init() throws ARuntimeException { /* stub */ }
-	
+
 }
