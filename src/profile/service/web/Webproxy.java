c7c19147c107ca4c4a4815ccf536661e60ca4683
Ed Geraghty <ed@geraghty.london>
2020-01-21 17:34:25 +0000

    Spaces to tabs

--- src/profile/service/web/Webproxy.java
+++ src/profile/service/web/Webproxy.java
@@ -94,56 +94,57 @@ public class Webproxy extends AStructuredProfile {
 		units.add(new DirUnit("nginx_ssl_include_dir", "proceed", "/etc/nginx/includes"));
 		final FileUnit sslConf = new FileUnit("nginx_ssl", "proceed", "/etc/nginx/includes/ssl_params");
 		units.add(sslConf);
-		sslConf.appendLine("    ssl_session_timeout 5m;");
-		sslConf.appendLine("    ssl_session_cache shared:SSL:10m;");
-		sslConf.appendLine("    ssl_session_tickets off;");
+		sslConf.appendLine("\tssl_session_timeout 5m;");
+		sslConf.appendLine("\tssl_session_cache shared:SSL:10m;");
+		sslConf.appendLine("\tssl_session_tickets off;");
 		sslConf.appendCarriageReturn();
-		sslConf.appendLine("    ssl_protocols TLSv1.2 TLSv1.3;");
-		sslConf.appendLine("    ssl_ciphers 'TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-256-GCM-SHA384:EECDH+CHACHA20:EECDH+AESGCM';");
-		sslConf.appendLine("    ssl_prefer_server_ciphers on;");
-		sslConf.appendLine("    ssl_ecdh_curve X25519:secp521r1:secp384r1;");
+		sslConf.appendLine("\tssl_protocols TLSv1.2 TLSv1.3;");
+		sslConf.appendLine(
+				"\tssl_ciphers 'TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-256-GCM-SHA384:EECDH+CHACHA20:EECDH+AESGCM';");
+		sslConf.appendLine("\tssl_prefer_server_ciphers on;");
+		sslConf.appendLine("\tssl_ecdh_curve X25519:secp521r1:secp384r1;");
 		sslConf.appendCarriageReturn();
-		sslConf.appendLine("    add_header Strict-Transport-Security 'max-age=63072000; includeSubDomains; preload';");
+		sslConf.appendLine("\tadd_header Strict-Transport-Security 'max-age=63072000; includeSubDomains; preload';");
 		sslConf.appendCarriageReturn();
-		sslConf.appendLine("    ssl_stapling on;");
-		sslConf.appendLine("    ssl_stapling_verify on;");
+		sslConf.appendLine("\tssl_stapling on;");
+		sslConf.appendLine("\tssl_stapling_verify on;");
 		for (final HostName resolver : getNetworkModel().getData().getUpstreamDNSServers()) {
-			sslConf.appendLine("    resolver " + resolver.asInetAddress() + " valid=300s;");
+			sslConf.appendLine("\tresolver " + resolver.asInetAddress() + " valid=300s;");
 		}
-		sslConf.appendLine("    resolver_timeout 5s;");
+		sslConf.appendLine("\tresolver_timeout 5s;");
 
 		// Now build our headers file
 		final FileUnit headersConf = new FileUnit("nginx_headers", "proceed", "/etc/nginx/includes/header_params");
-		headersConf.appendLine("    add_header X-Frame-Options                   'SAMEORIGIN' always;");
-		headersConf.appendLine("    add_header X-Content-Type-Options            'nosniff' always;");
-		headersConf.appendLine("    add_header X-XSS-Protection                  '1; mode=block' always;");
-		headersConf.appendLine("    add_header X-Download-Options                'noopen' always;");
-		headersConf.appendLine("    add_header X-Permitted-Cross-Domain-Policies 'none' always;");
+		headersConf.appendLine("\tadd_header X-Frame-Options                   'SAMEORIGIN' always;");
+		headersConf.appendLine("\tadd_header X-Content-Type-Options            'nosniff' always;");
+		headersConf.appendLine("\tadd_header X-XSS-Protection                  '1; mode=block' always;");
+		headersConf.appendLine("\tadd_header X-Download-Options                'noopen' always;");
+		headersConf.appendLine("\tadd_header X-Permitted-Cross-Domain-Policies 'none' always;");
 		headersConf.appendLine(
 				"    add_header Content-Security-Policy           'upgrade-insecure-requests; block-all-mixed-content; reflected-xss block;' always;");
 		headersConf.appendCarriageReturn();
-		headersConf.appendLine("    proxy_set_header Host               \\$host;");
-		headersConf.appendLine("    proxy_set_header X-Forwarded-Host   \\$host:\\$server_port;");
-		headersConf.appendLine("    proxy_set_header X-Forwarded-Server \\$host;");
-		headersConf.appendLine("    proxy_set_header X-Forwarded-Port   \\$server_port;");
-		headersConf.appendLine("    proxy_set_header X-Forwarded-Proto  https;");
+		headersConf.appendLine("\tproxy_set_header Host               \\$host;");
+		headersConf.appendLine("\tproxy_set_header X-Forwarded-Host   \\$host:\\$server_port;");
+		headersConf.appendLine("\tproxy_set_header X-Forwarded-Server \\$host;");
+		headersConf.appendLine("\tproxy_set_header X-Forwarded-Port   \\$server_port;");
+		headersConf.appendLine("\tproxy_set_header X-Forwarded-Proto  https;");
 		if (passThroughIps) {
 			headersConf.appendCarriageReturn();
 			headersConf.appendLine(
 					"\n#These headers pass real IP addresses to the backend - this may not be desired behaviour");
-			headersConf.appendLine("    proxy_set_header X-Forwarded-For    \\$proxy_add_x_forwarded_for;");
-			headersConf.appendLine("    proxy_set_header X-Real-IP          \\$remote_addr;");
+			headersConf.appendLine("\tproxy_set_header X-Forwarded-For    \\$proxy_add_x_forwarded_for;");
+			headersConf.appendLine("\tproxy_set_header X-Real-IP          \\$remote_addr;");
 		}
 		headersConf.appendCarriageReturn();
 		// Just in case, hide any extraneous "powered by" headers
-		headersConf.appendLine("    proxy_hide_header X-Powered-By;");
+		headersConf.appendLine("\tproxy_hide_header X-Powered-By;");
 
 		// And our forced (301) SSL upgrade
 		final FileUnit sslConfig = new FileUnit("nginx_ssl_config", "nginx_installed",
 				Nginx.DEFAULT_CONFIG_FILE.toString());
 		sslConfig.appendLine("server {");
-		sslConfig.appendLine("    listen 80 default;");
-		sslConfig.appendLine("    return 301 https://\\$host\\$request_uri;");
+		sslConfig.appendLine("\tlisten 80 default;");
+		sslConfig.appendLine("\treturn 301 https://\\$host\\$request_uri;");
 		sslConfig.appendLine("}");
 
 		this.webserver.addLiveConfig(sslConfig);
@@ -193,7 +194,7 @@ public class Webproxy extends AStructuredProfile {
 				}
 				nginxConf.appendText(";");
 
-				nginxConf.appendLine("    server_name " + backendLabel + "." + domain);
+				nginxConf.appendLine("\tserver_name " + backendLabel + "." + domain);
 
 				if (cnames != null) {
 					for (final String cname : cnames) {
@@ -205,35 +206,35 @@ public class Webproxy extends AStructuredProfile {
 				nginxConf.appendLine(";");
 
 				// Let's separate the logs out...
-				nginxConf.appendLine("    access_log " + logDir + "access.log;"); // main buffer=16k;");
-				nginxConf.appendLine("    error_log  " + logDir + "error.log;"); // main buffer=16k;");
+				nginxConf.appendLine("\taccess_log " + logDir + "access.log;"); // main buffer=16k;");
+				nginxConf.appendLine("\terror_log  " + logDir + "error.log;"); // main buffer=16k;");
 				nginxConf.appendCarriageReturn();
 				// We use the Let's Encrypt naming convention here, in case we want to install
 				// it later
-				nginxConf.appendLine("    include /etc/nginx/includes/ssl_params;");
-				nginxConf.appendLine("    include /etc/nginx/includes/header_params;");
-				nginxConf.appendLine("    ssl_certificate /media/data/tls/" + backendLabel + "/fullchain.pem;");
-				nginxConf.appendLine("    ssl_certificate_key /media/data/tls/" + backendLabel + "/privkey.pem;");
-				nginxConf.appendLine("    ssl_trusted_certificate /media/data/tls/" + backendLabel + "/stapling.pem;");
+				nginxConf.appendLine("\tinclude /etc/nginx/includes/ssl_params;");
+				nginxConf.appendLine("\tinclude /etc/nginx/includes/header_params;");
+				nginxConf.appendLine("\tssl_certificate /media/data/tls/" + backendLabel + "/fullchain.pem;");
+				nginxConf.appendLine("\tssl_certificate_key /media/data/tls/" + backendLabel + "/privkey.pem;");
+				nginxConf.appendLine("\tssl_trusted_certificate /media/data/tls/" + backendLabel + "/stapling.pem;");
 				nginxConf.appendCarriageReturn();
-				nginxConf.appendLine("    location / {");
+				nginxConf.appendLine("\tlocation / {");
 				// for (final JsonValue source :
 				// getNetworkModel().getData().getData().getJsonArray(backend, "allow")) {
 				// nginxConf.appendLine(" allow " + source + ";");
 				// nginxConf.appendLine(" deny all;");
 				// }
-				nginxConf.appendLine("        proxy_pass              http://" + backendObj.getIP() + ";");
-				nginxConf.appendLine("        proxy_request_buffering off;");
-				nginxConf.appendLine("        proxy_buffering         off;");
-				nginxConf.appendLine("        client_max_body_size    0;");
-				nginxConf.appendLine("        proxy_http_version      1.1;");
-				nginxConf.appendLine("        proxy_connect_timeout   3000;");
-				nginxConf.appendLine("        proxy_send_timeout      3000;");
-				nginxConf.appendLine("        proxy_read_timeout      3000;");
-				nginxConf.appendLine("        send_timeout            3000;");
-				nginxConf.appendLine("    }");
+				nginxConf.appendLine("\t\tproxy_pass              http://" + backendObj.getIP() + ";");
+				nginxConf.appendLine("\t\tproxy_request_buffering off;");
+				nginxConf.appendLine("\t\tproxy_buffering         off;");
+				nginxConf.appendLine("\t\tclient_max_body_size    0;");
+				nginxConf.appendLine("\t\tproxy_http_version      1.1;");
+				nginxConf.appendLine("\t\tproxy_connect_timeout   3000;");
+				nginxConf.appendLine("\t\tproxy_send_timeout      3000;");
+				nginxConf.appendLine("\t\tproxy_read_timeout      3000;");
+				nginxConf.appendLine("\t\tsend_timeout            3000;");
+				nginxConf.appendLine("\t}");
 				nginxConf.appendCarriageReturn();
-				nginxConf.appendLine("    include /media/data/nginx_custom_blocks/" + backendLabel + ".conf;");
+				nginxConf.appendLine("\tinclude /media/data/nginx_custom_blocks/" + backendLabel + ".conf;");
 				nginxConf.appendLine("}");
 
 				this.webserver.addLiveConfig(nginxConf);
