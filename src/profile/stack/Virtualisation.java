40a2231efb0631ed8ee3283aa45593ac09255768
Ed <ed@geraghty.london>
2020-02-06 23:51:24 +0000

    move to en_GB.UTF-8

--- src/profile/stack/Virtualisation.java
+++ src/profile/stack/Virtualisation.java
@@ -178,8 +178,8 @@ public class Virtualisation extends AStructuredProfile {
 		
 		//Debian installer options.
 		preseed += "\n";
-		preseed += "d-i debian-installer/locale string en_US\n";
-		preseed += "d-i keyboard-configuration/xkb-keymap select us\n";
+		preseed += "d-i debian-installer/locale string en_GB.UTF-8\n";
+		preseed += "d-i keyboard-configuration/xkb-keymap select gb\n";
 		preseed += "d-i clock-setup/ntp boolean false\n";
 		preseed += "d-i netcfg/target_network_config select ifupdown\n";
 		preseed += "d-i netcfg/choose_interface select auto\n";
