5e253fd800fc43ed3d79b0f0577a7dad26e01033
Ed <ed@geraghty.london>
2020-07-06 13:46:53 +0100

    Refactor BondInterfaceModel

--- src/core/model/machine/configuration/networking/BondInterfaceModel.java
+++ src/core/model/machine/configuration/networking/BondInterfaceModel.java
@@ -7,7 +7,12 @@
  */
 package core.model.machine.configuration.networking;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+import core.data.machine.configuration.NetworkInterfaceData;
 import core.data.machine.configuration.NetworkInterfaceData.Inet;
+import core.model.network.NetworkModel;
 import core.unit.fs.FileUnit;
 
 /**
@@ -15,40 +20,41 @@ import core.unit.fs.FileUnit;
  * interface to add to a trunk.
  */
 public class BondInterfaceModel extends NetworkInterfaceModel {
-	private final NetworkInterfaceModel trunk;
+	private BondModel bond;
 
-	public BondInterfaceModel(String name, NetworkInterfaceModel trunk) {
-		super(name);
-
-		this.trunk = trunk;
+	public BondInterfaceModel(NetworkInterfaceData myData, NetworkModel networkModel) {
+		super(myData, networkModel);
+		
 		super.setInet(Inet.MANUAL);
+		super.setReqdForOnline(true);
+		super.setWeighting(0);
+		super.setARP(false);
+		
+		this.bond = null;
 	}
 
 	@Override
-	public FileUnit getNetworkFile() {
-		final FileUnit network = new FileUnit(getIface() + "_network", "proceed", "/etc/systemd/network/00-" + getIface() + ".network");
-		network.appendLine("[Match]");
-		network.appendLine("Name=" + getIface());
+	public Optional<FileUnit> getNetworkFile() {
+		final FileUnit network = super.getNetworkFile().get();
+		
 		network.appendCarriageReturn();
+		network.appendLine("Bond=" + getBond().getIface());
 
-		network.appendLine("[Link]");
-		network.appendLine("RequiredForOnline=yes");
-		network.appendLine("ARP=no");
-		network.appendCarriageReturn();
-
-		network.appendLine("[Network]");
-		network.appendLine("Bond=" + getBond());
-
-		return network;
+		return Optional.of(network);
 	}
 
-	private String getBond() {
-		return this.trunk.getIface();
+	private BondModel getBond() {
+		return this.bond;
+	}
+	
+	public void setBond(BondModel bond) {
+		assertNotNull(bond);
+		
+		this.bond = bond;
 	}
 
 	@Override
-	public FileUnit getNetDevFile() {
-		// Don't need a NetDev for a Bonded Link
-		return null;
+	public Optional<FileUnit> getNetDevFile() {
+		return Optional.empty(); // Don't need a NetDev for a Bonded Link
 	}
 }
