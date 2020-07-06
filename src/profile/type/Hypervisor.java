8396a2e074f0de37fe0690398cd6edd6d604e0af
Ed <ed@geraghty.london>
2020-07-06 13:15:06 +0100

    HyperVisor -> Hypervisor
    ...and all the changes it brings.

--- src/profile/type/Hypervisor.java
+++ src/profile/type/Hypervisor.java
@@ -7,6 +7,7 @@
  */
 package profile.type;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -15,7 +16,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.json.stream.JsonParsingException;
@@ -23,7 +27,7 @@ import javax.swing.JOptionPane;
 import core.StringUtils;
 import core.data.machine.AMachineData.MachineType;
 import core.data.machine.HypervisorData;
-import core.data.machine.UserDeviceData;
+import core.data.machine.ServerData;
 import core.data.machine.configuration.NetworkInterfaceData;
 import core.data.machine.configuration.DiskData.Medium;
 import core.data.machine.configuration.NetworkInterfaceData.Direction;
@@ -37,7 +41,10 @@ import core.exception.runtime.InvalidServerModelException;
 import core.exec.network.APassphrase;
 import core.exec.network.OpenKeePassPassphrase;
 import core.iface.IUnit;
+import core.model.machine.AMachineModel;
+import core.model.machine.HypervisorModel;
 import core.model.machine.ServerModel;
+import core.model.machine.ServiceModel;
 import core.model.machine.configuration.DiskModel;
 import core.model.machine.configuration.networking.DHCPClientInterfaceModel;
 import core.model.machine.configuration.networking.NetworkInterfaceModel;
@@ -52,7 +59,8 @@ import core.unit.fs.FileDownloadUnit;
 import core.unit.pkg.InstalledUnit;
 import inet.ipaddr.IPAddress;
 import profile.HypervisorScripts;
-import profile.stack.Virtualbox;
+import profile.hypervisor.AHypervisorProfile;
+import profile.hypervisor.Virtualbox;
 
 /**
  * This is the representation of your HyperVisor itself.
@@ -60,32 +68,30 @@ import profile.stack.Virtualbox;
  * These are things which should be done on a HyperVisor machine, regardless of
  * what hypervisor layer it's actually running
  */
-public class HyperVisor extends AStructuredProfile {
+public class Hypervisor extends AStructuredProfile {
 
-	private final Virtualbox hypervisor;
+	private final AHypervisorProfile virtualbox;
 	private final HypervisorScripts scripts;
 	
-	private Map<String, ServerModel> services;
+	private Set<ServiceModel> services;
 
 	/**
 	 * Create a new HyperVisor box, with initialised NICs, and initialise the
 	 * virtualisation layer itself, including the building of Service machines
 	 * 
-	 * @param label
+	 * @param myData
 	 * @param networkModel
-	 * @throws InvalidServerModelException
 	 * @throws JsonParsingException
 	 * @throws ADataException
+	 * @throws InvalidMachineModelException 
 	 */
-	public HyperVisor(String label, NetworkModel networkModel) throws InvalidServerModelException, JsonParsingException, ADataException {
-		super(label, networkModel);
+	public Hypervisor(HypervisorModel me) throws JsonParsingException, ADataException, InvalidMachineModelException {
+		super(me);
 
-		final ServerModel me = getNetworkModel().getServerModel(getLabel());
-	
-		if (!me.isRouter()) {
-			try {
-				if (networkModel.getData().getNetworkInterfaces(getLabel()).get(Direction.WAN) != null) {
-					for (final NetworkInterfaceData wanNic : networkModel.getData().getNetworkInterfaces(getLabel())
+		if (!me.isType(MachineType.ROUTER)) {
+/*			try {
+				if (getMachineModel().getNetworkInterfaces().get(Direction.WAN) != null) {
+					for (final NetworkInterfaceData wanNic : getMachineModel().getNetworkInterfaces()
 							.get(Direction.WAN).values()) {
 						NetworkInterfaceModel link = null;
 	
@@ -130,38 +136,31 @@ public class HyperVisor extends AStructuredProfile {
 			} catch (final IOException e) {
 				e.printStackTrace();
 			}
+			*/
 		}
 		
-		this.hypervisor = new Virtualbox(label, networkModel);
-		this.scripts = new HypervisorScripts(label, networkModel);
-		this.services = null;
-	}
-
-	/**
-	 * Add a Service to this HyperVisor
-	 * @param label the Service's label
-	 * @param service the Service model to add
-	 */
-	public void addService(String label, ServerModel service) {
-		if (this.services == null) {
-			this.services = new LinkedHashMap<>();
-		}
+		this.virtualbox = new Virtualbox(me);
+		this.scripts = new HypervisorScripts(me);
 		
-		this.services.put(label, service);
+		addServices();
 	}
 
-	public Map<String, ServerModel> getServices() {
+	private void addServices() throws InvalidMachineModelException {
+		this.services = getServerModel().getServices();
+	}
+		
+	public Set<ServiceModel> getServices() {
 		return this.services;
 	}
 
 	@Override
-	protected Collection<IUnit> getInstalled() throws InvalidServerException {
+	public Collection<IUnit> getInstalled() throws AThornSecException {
 		final Collection<IUnit> units = new ArrayList<>();
 
-		units.addAll(this.hypervisor.getInstalled());
+		units.addAll(this.virtualbox.getInstalled());
 		units.addAll(this.scripts.getInstalled());
 
-		units.add(new DirUnit("media_dir", "proceed", getNetworkModel().getData().getHypervisorThornsecBase(getLabel()).toString()));
+		units.add(new DirUnit("thornsec_base_dir", "proceed", getServerModel().getVMBase().getAbsolutePath()));
 
 		units.add(new InstalledUnit("whois", "proceed", "whois"));
 		units.add(new InstalledUnit("tmux", "proceed", "tmux"));
@@ -171,10 +170,10 @@ public class HyperVisor extends AStructuredProfile {
 	}
 
 	@Override
-	public Collection<IUnit> getPersistentConfig() throws InvalidServerModelException, InvalidServerException {
+	public Collection<IUnit> getPersistentConfig() throws AThornSecException {
 		final Collection<IUnit> units = new ArrayList<>();
 
-		units.addAll(this.hypervisor.getPersistentConfig());
+		units.addAll(this.virtualbox.getPersistentConfig());
 		units.addAll(this.scripts.getPersistentConfig());
 
 		return units;
@@ -184,16 +183,16 @@ public class HyperVisor extends AStructuredProfile {
 	public Collection<IUnit> getPersistentFirewall() throws AThornSecException {
 		final Collection<IUnit> units = new ArrayList<>();
 
-		units.addAll(this.hypervisor.getPersistentFirewall());
+		units.addAll(this.virtualbox.getPersistentFirewall());
 		units.addAll(this.scripts.getPersistentFirewall());
 
-		getNetworkModel().getServerModel(getLabel()).addEgress("gensho.ftp.acc.umu.se:80");
-		getNetworkModel().getServerModel(getLabel()).addEgress("github.com:443");
+		//getMachineModel().addEgress("gensho.ftp.acc.umu.se:80");
+		//getMachineModel().addEgress("github.com:443");
 
 		return units;
 	}
 
-	private Collection<IUnit> getISODownloadUnits(String url, String checksum) throws InvalidServerException {
+	private Collection<IUnit> getISODownloadUnits() throws InvalidServerException {
 		final Collection<IUnit> units = new ArrayList<>();
 
 		String filename = null;
@@ -209,10 +208,10 @@ public class HyperVisor extends AStructuredProfile {
 		}
 
 		units.add(new FileDownloadUnit(cleanedFilename, "metal_genisoimage_installed", url,
-				getNetworkModel().getData().getHypervisorThornsecBase(getLabel()) + "/" + filename,
+				getServerModel().getVMBase().getAbsolutePath() + "/" + filename,
 				"The Debian net install ISO couldn't be downloaded.  Please check the URI in your config."));
 		units.add(new FileChecksumUnit(cleanedFilename, cleanedFilename + "_downloaded", Checksum.SHA512,
-				getNetworkModel().getData().getHypervisorThornsecBase(getLabel()) + "/" + filename, checksum,
+				getServerModel().getVMBase().getAbsolutePath() + "/" + filename, checksum,
 				"The sha512 sum of the Debian net install in your config doesn't match what has been downloaded."
 				+ " This could mean your connection is man-in-the-middle'd, that the download was corrupted,"
 				+ " or it could just be that the file has been updated on the server."
@@ -222,40 +221,8 @@ public class HyperVisor extends AStructuredProfile {
 		return units;
 	}
 	
-	private Collection<IUnit> getUserPasswordUnits(String service) throws ARuntimeException, ADataException {
-		final Collection<IUnit> units = new ArrayList<>();
-
-		String password = "";
-		final APassphrase pass = new OpenKeePassPassphrase(service, getNetworkModel());
-
-		if (pass.init()) {
-			password = pass.getPassphrase();
-			password = Pattern.quote(password); // Turn special characters into literal so they don't get parsed out
-			password = password.substring(2, password.length() - 2).trim(); // Remove '\Q' and '\E' from beginning/end since we're not using this as a regex
-			password = password.replace("\"", "\\\""); // Also, make sure quote marks are properly escaped!
-		}
-		else {
-			password = ((UserDeviceData)getNetworkModel().getData().getUserDevices().get(getNetworkModel().getData().getUser())).getDefaultPassphrase();
-		}
-		
-		if (password == null || password.isEmpty()) {
-			password = service;
-		}
-
-		//if (pass.isADefaultPassphrase()) {
-		//}
-
-		units.add(new SimpleUnit(service + "_password", "proceed",
-				service.toUpperCase() + "_PASSWORD=$(printf \"" + password + "\" | mkpasswd -s -m md5)",
-				"echo $" + service.toUpperCase() + "_PASSWORD", "", "fail",
-				"Couldn't set the passphrase for " + service + "."
-				+ " You won't be able to configure this service."));
-		
-		return units;
-	}
-	
-	private String getNetworkBridge() throws InvalidServerException, InvalidServerModelException {
-		if (getNetworkModel().getServerModel(getLabel()).isRouter()) {
+	private String getNetworkBridge() throws InvalidServerException, InvalidMachineModelException {
+		if (getMachineModel().isType(MachineType.ROUTER)) {
 			return MachineType.SERVER.toString();
 		}
 
@@ -274,26 +241,18 @@ public class HyperVisor extends AStructuredProfile {
 	public Collection<IUnit> getLiveConfig() throws AThornSecException {
 		final Collection<IUnit> units = new ArrayList<>();
 
-		((HypervisorData)getNetworkModel().getData().getServers().get(getLabel())).getServices().forEach(server -> {
-			try {
-				addService(server.getLabel(), getNetworkModel().getServerModel(server.getLabel()));
-			} catch (InvalidServerModelException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
-		
-		for (final String service : getServices().keySet()) {
-			units.addAll(getISODownloadUnits(getNetworkModel().getData().getDebianIsoUrl(service), getNetworkModel().getData().getDebianIsoSha512(service)));
-			units.addAll(getUserPasswordUnits(service));
+		for (final ServiceModel service : getServices()) {
+			units.addAll(service.getOS().getISODownloadUnits();
+			units.addAll(getUserPasswordUnits());
 
-			try {
-				units.addAll(this.hypervisor.buildIso(service));
-			} catch (InvalidServerException | InvalidServerModelException | NoValidUsersException | MalformedURLException | URISyntaxException e) {
+			//try {
+				//TODO
+				//units.addAll(this.hypervisor.buildIso(service));
+			//} catch (InvalidServerException | InvalidServerModelException | NoValidUsersException | MalformedURLException | URISyntaxException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			units.addAll(this.hypervisor.buildServiceVm(service, getNetworkBridge()));
+			//	e.printStackTrace();
+			//}
+			units.addAll(virtualbox.buildServiceVm(service, getNetworkBridge()));
 			units.addAll(getDisksFormattedUnits(service));
 			units.addAll(getDiskLoopbackUnits(service));
 		}
@@ -307,7 +266,7 @@ public class HyperVisor extends AStructuredProfile {
 	private Collection<IUnit> getDisksFormattedUnits(String service) throws InvalidMachineModelException {
 		final Collection<IUnit> units = new ArrayList<>();
 		
-		getNetworkModel().getServiceModel(service).getDisks().forEach((label, disk) -> {
+		((ServiceModel)getNetworkModel().getMachineModel(service)).getDisks().forEach((label, disk) -> {
 			// For now, do all this as root. We probably want to move to another user, idk
 			units.add(new SimpleUnit(service + "_" + label + "_disk_formatted", "proceed",
 					"", //No config to do here
@@ -321,7 +280,7 @@ public class HyperVisor extends AStructuredProfile {
 	private Collection<IUnit> getDiskLoopbackUnits(String service) throws InvalidMachineModelException {
 		final Collection<IUnit> units = new ArrayList<>();
 		
-		for (DiskModel disk : getNetworkModel().getServiceModel(service).getDisks().values().stream().filter(disk -> disk.getMedium() == Medium.DISK).toArray(DiskModel[]::new)) {
+		for (DiskModel disk : ((ServiceModel)getNetworkModel().getMachineModel(service)).getDisks().values().stream().filter(disk -> disk.getMedium() == Medium.DISK).toArray(DiskModel[]::new)) {
 			// For now, do all this as root. We probably want to move to another user, idk
 			
 			units.add(new SimpleUnit(service + "_" + disk.getLabel() + "_disk_loopback_mounted", service + "_" + disk.getLabel() + "_disk_formatted",
@@ -330,9 +289,14 @@ public class HyperVisor extends AStructuredProfile {
 							+ " --ro" // _MOUNT THE DISK READ ONLY_
 							+ " " + disk.getFilePath() + "/live/" + "'",
 					"sudo mount | grep " + disk.getFilePath(), "", "fail",
-					"I was unable to loopback mount the " + disk.getLabel() + " disk for " + service + " in " + getLabel() + "."));
+					"I was unable to loopback mount the " + disk.getLabel() + " disk for " + service + " in " + getMachineModel().getLabel() + "."));
 		}
 		
 		return units;
+	}
+	
+	@Override
+	public HypervisorModel getServerModel() {
+		return (HypervisorModel) getServerModel();
 	}
 }
