f2918399c4b62a4b07aa7f0d71ea4bce04e41c5e
Ed <ed@geraghty.london>
2020-08-05 15:50:25 +0100

    remove unused imports

--- src/core/model/machine/UserDeviceModel.java
+++ src/core/model/machine/UserDeviceModel.java
@@ -7,15 +7,11 @@
  */
 package core.model.machine;
 
-import java.io.IOException;
 import java.util.Collection;
 
 import core.data.machine.UserDeviceData;
 import core.exception.AThornSecException;
-
-import core.exception.data.ADataException;
-import core.exception.runtime.InvalidDeviceModelException;
-import core.exception.runtime.InvalidServerModelException;
+import core.exception.data.InvalidPortException;
 import core.iface.IUnit;
 import core.model.network.NetworkModel;
 
