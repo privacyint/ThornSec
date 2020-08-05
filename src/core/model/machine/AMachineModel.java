93af95cd63f7e3643d7fbbf33c647fa58d7bb319
Ed <ed@geraghty.london>
2020-08-05 14:23:32 +0100

    Major refactor of AMachineModel
    I'd love to have committed this as one's supposed to, but as you can see a lot has changed...

--- src/core/model/machine/AMachineModel.java
+++ src/core/model/machine/AMachineModel.java
@@ -7,215 +7,195 @@
  */
 package core.model.machine;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
-
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-
-import core.data.machine.AMachineData.Encapsulation;
+import com.metapossum.utils.scanner.reflect.ClassesInPackageScanner;
+import core.StringUtils;
+import core.data.machine.AMachineData;
+import core.data.machine.AMachineData.MachineType;
+import core.data.machine.ServerData;
+import core.data.machine.configuration.NetworkInterfaceData;
+import core.data.machine.configuration.NetworkInterfaceData.Inet;
+import core.data.machine.configuration.TrafficRule;
 import core.exception.AThornSecException;
-import core.exception.data.ADataException;
-import core.exception.data.InvalidPortException;
+import core.exception.runtime.InvalidProfileException;
 import core.iface.IUnit;
 import core.model.AModel;
+import core.model.machine.configuration.networking.DHCPClientInterfaceModel;
 import core.model.machine.configuration.networking.NetworkInterfaceModel;
+import core.model.machine.configuration.networking.StaticInterfaceModel;
 import core.model.network.NetworkModel;
+import core.profile.AProfile;
 import inet.ipaddr.AddressStringException;
 import inet.ipaddr.HostName;
 import inet.ipaddr.IPAddress;
 import inet.ipaddr.IncompatibleAddressException;
 import inet.ipaddr.MACAddressString;
 import inet.ipaddr.mac.MACAddress;
+import profile.type.AMachine;
+import profile.type.Device;
+import profile.type.Server;
+import profile.type.Service;
 
 /**
  * This class represents a Machine on our network.
+ * 
+ * Everything on our network is a descendant from this class, 
  *
  * This is where we stash our various networking rules
  */
 public abstract class AMachineModel extends AModel {
-	private ArrayList<NetworkInterfaceModel> networkInterfaces;
+	private Set<NetworkInterfaceModel> networkInterfaces;
 
-	private final HostName domain;
-	private final Collection<String> cnames;
+	private HostName domain;
+	private Set<String> cnames;
 
 	private InternetAddress emailAddress;
 
-	private Integer cidr;
-
+	private Map<MachineType, AProfile> types;
+	
 	private Boolean throttled;
 
-	private final Map<Encapsulation, Collection<Integer>> listens;
+	private Set<TrafficRule> firewallRules;
+	private Set<IPAddress> externalIPs;
 
-	private Collection<String> forwards;
-	private Collection<HostName> ingresses;
-	private Collection<HostName> egresses;
-	private final Collection<IPAddress> externalIPs;
+	AMachineModel(AMachineData myData, NetworkModel networkModel) throws AThornSecException {
+		super(myData, networkModel);
 
-	private final Map<String, Collection<Integer>> dnats;
+		setTypesFromData(myData);
+		setEmailFromData(myData);
+		setNICsFromData(myData);
+		setDomainFromData(myData);
+		setCNAMEsFromData(myData);
+		setExternalIPsFromData(myData);
+		setFirewallFromData(myData);
+	}
 
-	AMachineModel(String label, NetworkModel networkModel) throws AddressException, JsonParsingException, ADataException, IOException {
-		super(label, networkModel);
-
-		this.networkInterfaces = new ArrayList<>();
+	private void setFirewallFromData(AMachineData myData) {
+		this.firewallRules = myData.getTrafficRules();
 		
-		this.emailAddress = getNetworkModel().getData().getEmailAddress(getLabel());
-
-		this.domain = getNetworkModel().getData().getDomain(getLabel());
-		this.cnames = getNetworkModel().getData().getCNAMEs(getLabel());
-
-		this.externalIPs = getNetworkModel().getData().getExternalIPs(getLabel());
-
-		this.throttled = getNetworkModel().getData().isThrottled(getLabel());
-
-		this.listens = getNetworkModel().getData().getListens(getLabel());
-		this.ingresses = getNetworkModel().getData().getIngresses(getLabel());
-		this.egresses = getNetworkModel().getData().getEgresses(getLabel());
-		this.forwards = getNetworkModel().getData().getForwards(getLabel());
-		this.dnats = getNetworkModel().getData().getDNATs(getLabel());
+		//assertNotNull(this.firewallRules);
+	}
+	
+	public void addFirewallRule(TrafficRule rule) {
+		//assertNotNull(rule);
+		
+		this.firewallRules.add(rule);
 	}
 
-//	final private NetworkInterfaceModel ifaceDataToModel(NetworkInterfaceData ifaceData) {
-//		final NetworkInterfaceModel ifaceModel = new NetworkInterfaceModel(getLabel(), getNetworkModel());
-//
-//		ifaceModel.setIface(ifaceData.getIface());
-//		ifaceModel.setAddress(ifaceData.getAddress());
-//		// ifaceModel.setMACVLANs(ifaceData.getMACVLANs());
-//		ifaceModel.setBroadcast(ifaceData.getBroadcast());
-//		ifaceModel.setComment(ifaceData.getComment());
-//		ifaceModel.setGateway(ifaceData.getGateway());
-//		ifaceModel.setInet(ifaceData.getInet());
-//		ifaceModel.setMac(ifaceData.getMAC());
-//		ifaceModel.setNetmask(ifaceData.getNetmask());
-//		ifaceModel.setSubnet(ifaceData.getSubnet());
-//
-//		return ifaceModel;
-//	}
-
-	public final Integer getCIDR() {
-		return this.cidr;
+	private void setExternalIPsFromData(AMachineData myData) {
+		this.externalIPs = myData.getExternalIPs();	
+		
+		//assertNotNull(this.externalIPs);
 	}
 
-	public final void setCIDR(Integer cidr) {
-		this.cidr = cidr;
+	private void setCNAMEsFromData(AMachineData myData) {
+		this.cnames = myData.getCNAMEs().orElse(new LinkedHashSet<>());
+	}
+
+	private void setDomainFromData(AMachineData myData) {
+		this.domain = myData.getDomain().orElse(new HostName("lan"));
+	}
+
+	/**
+	 * Set up and initialise our various NICs as set from our Data
+	 * @param myData
+	 * @throws AThornSecException
+	 */
+	private void setNICsFromData(AMachineData myData) throws AThornSecException {
+		if (myData.getNetworkInterfaces().isEmpty()) {
+			return;
+		}
+		
+		for (NetworkInterfaceData nicData : myData.getNetworkInterfaces().get().values()) {
+			NetworkInterfaceModel nicModel = buildNICFromData(nicData);
+			nicModel.init();
+			this.addNetworkInterface(nicModel);
+		}
+	}
+
+	private NetworkInterfaceModel buildNICFromData(NetworkInterfaceData ifaceData) throws AThornSecException {
+		NetworkInterfaceModel nicModel = null;
+		switch ((ifaceData.getInet() != null)
+					? ifaceData.getInet()
+					: Inet.STATIC)
+		{
+			case STATIC:
+				nicModel = new StaticInterfaceModel(ifaceData, getNetworkModel());
+				break;
+			case DHCP:
+				nicModel = new DHCPClientInterfaceModel(ifaceData, getNetworkModel());
+				break;
+//			case PPP:
+//				nicModel = new PPPClientInterfaceModel(label);
+//				break;
+			default:
+				break;
+		}
+
+		return nicModel;
+	}
+
+	private void setEmailFromData(AMachineData myData) {
+		try {
+			this.emailAddress = myData.getEmailAddress()
+					.orElse(new InternetAddress(getLabel() + "@" + getDomain()));
+		} catch (AddressException e) {
+			;; // You should not be able to get here. 
+		}
+	}
+
+	private void setTypesFromData(AMachineData myData) {
+		this.types = new LinkedHashMap<>();
+		
+		myData.getTypes().forEach(type -> {
+			addType(type);
+		});
+	}
+
+	public final void addType(MachineType type) {
+//		//assertNotNull(type);
+		
+		switch (type) {
+			case DEVICE:
+				addType(type, new Device((ADeviceModel)this));
+				break;
+			case SERVER:
+				addType(type, new Server((ServerModel)this));
+				break;
+			case SERVICE:
+				addType(type, new Service((ServiceModel)this));
+				break;
+			default:
+				break;
+		}
+	}
+	
+	public final void addType(MachineType type, AMachine profile) {
+		this.types.put(type, profile);
 	}
 
 	public final void addNetworkInterface(NetworkInterfaceModel ifaceModel) {
+		if (this.networkInterfaces == null) {
+			this.networkInterfaces = new LinkedHashSet<>();
+		}
+		
 		this.networkInterfaces.add(ifaceModel);
 	}
 
 	public final Collection<NetworkInterfaceModel> getNetworkInterfaces() {
 		return this.networkInterfaces;
-	}
-
-	public final void addIngress(String... sources) {
-		for (final String source : sources) {
-			addIngress(new HostName(source));
-		}
-	}
-
-	public final void addIngress(HostName... sources) {
-		if (this.ingresses == null) {
-			this.ingresses = new HashSet<>();
-		}
-
-		for (final HostName source : sources) {
-			this.ingresses.add(source);
-		}
-	}
-
-	public final Collection<HostName> getIngresses() {
-		return this.ingresses;
-	}
-
-	public final void addDNAT(String destination, Integer... ports) throws InvalidPortException {
-		Collection<Integer> dnats = this.dnats.get(destination);
-
-		if (dnats == null) {
-			dnats = new HashSet<>();
-		}
-
-		for (final Integer port : ports) {
-			if ((port < 1) || (port > 65535)) {
-				throw new InvalidPortException(port);
-			}
-
-			dnats.add(port);
-		}
-
-		this.dnats.put(destination, dnats);
-	}
-
-	public final Map<String, Collection<Integer>> getDNAT() {
-		return this.dnats;
-	}
-
-	public final void addListen(Encapsulation enc, Integer... ports) throws InvalidPortException {
-		Collection<Integer> listening = this.listens.get(enc);
-
-		if (listening == null) {
-			listening = new HashSet<>();
-		}
-
-		for (final Integer port : ports) {
-			if ((port < 1) || (port > 65535)) {
-				throw new InvalidPortException(port);
-			}
-
-			listening.add(port);
-		}
-
-		this.listens.put(enc, listening);
-	}
-
-	public final Map<Encapsulation, Collection<Integer>> getListens() {
-		return this.listens;
-	}
-
-	public final void addEgress(String... egresses) {
-		for (final String egress : egresses) {
-			addEgress(new HostName(egress));
-		}
-	}
-
-	public final void addEgress(HostName... egresses) {
-		if (this.egresses == null) {
-			this.egresses = new LinkedHashSet<>();
-		}
-
-		for (final HostName egress : egresses) {
-			this.egresses.add(egress);
-		}
-
-	}
-
-	public final Collection<HostName> getEgresses() {
-		return this.egresses;
-	}
-
-	public final Map<String, Collection<Integer>> getRequiredDnat() {
-		return this.dnats;
-	}
-
-	public final void addForward(String... destinations) {
-		if (this.forwards == null) {
-			this.forwards = new HashSet<>();
-		}
-
-		for (final String destination : destinations) {
-			this.forwards.add(destination);
-		}
-	}
-
-	public final Collection<String> getForwards() {
-		return this.forwards;
 	}
 
 	public final Collection<IPAddress> getExternalIPs() {
